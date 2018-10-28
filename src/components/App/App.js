e45dab0eef5c2982ebb6df6825f92ff999e54b06
Leonardo Cano <leocano4242@gmail.com>
2018-10-28 00:01:41 -0300

    Moving MainContnet

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import './App.css'
 
-import MainContent from '../MainContent/MainContent'
+import MainContent from '../generic/MainContent/MainContent'
 
 const App = props => {
   return <MainContent>Hello World!</MainContent>

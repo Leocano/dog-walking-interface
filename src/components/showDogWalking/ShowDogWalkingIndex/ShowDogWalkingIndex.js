7684076b6a22200785f25eb8a20b5c51c626e5fe
Leonardo Cano <leocano4242@gmail.com>
2018-10-28 16:53:54 -0300

    Exibindo dados do Dog Walking

--- src/components/showDogWalking/ShowDogWalkingIndex/ShowDogWalkingIndex.js
+++ src/components/showDogWalking/ShowDogWalkingIndex/ShowDogWalkingIndex.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import { inject, observer } from 'mobx-react'
-import { withRouter } from 'react-router-dom'
 
 import Container from '../../generic/Container/Container'
 import Box from '../../generic/Box/Box'
 import Title from '../../generic/Title/Title'
+import Loader from '../../generic/Loader/Loader'
+import DogWalkingInfo from '../DogWalkingInfo/DogWalkingInfo'
 
 const ShowDogWalkingIndex = inject('showDogWalkingStore')(observer(
   class ShowDogWalkingIndex extends Component {
@@ -14,10 +15,14 @@ const ShowDogWalkingIndex = inject('showDogWalkingStore')(observer(
     }
 
     render () {
+      const { isFetching } = this.props.showDogWalkingStore.state
       return (
         <Container>
           <Box>
             <Title>Dog Walking - {this.props.match.params.id}</Title>
+            <Loader isLoading={isFetching}>
+              <DogWalkingInfo />
+            </Loader>
           </Box>
         </Container>
       )
